f2070107fcc7dba5584a6ab9ea7219eb91ba52ed
Troy Beaudin <troybeaudin@beaudinit.ca>
2024-11-05 10:53:24 -0500

    Updated versions

--- versions.tf
+++ versions.tf
@@ -1,11 +1,11 @@
 
 terraform {
-  required_version = "~> 1.0"
+  required_version = "~> 1.6"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.40"
+      version = "~> 5.22.0"
     }
   }
 }
